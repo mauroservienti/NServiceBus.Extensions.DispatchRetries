07ae8626a3269c9ed1598916a93bdd01eb951ccf
Mauro Servienti <mauro.servienti@gmail.com>
2020-09-13 11:10:14 +0200

    basic infrastructure

---
+++ src/NServiceBus.Extensions.DispatchRetries/DispatchRetriesBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus.Pipeline;
+using Polly;
+
+namespace NServiceBus.Extensions.DispatchRetries
+{
+    public class DispatchRetriesBehavior : Behavior<IOutgoingPhysicalMessageContext>
+    {
+        private readonly AsyncPolicy _defaultRetryPolicy;
+
+        public DispatchRetriesBehavior(AsyncPolicy defaultRetryPolicy)
+        {
+            _defaultRetryPolicy = defaultRetryPolicy;
+        }
+
+        public override Task Invoke(IOutgoingPhysicalMessageContext context, Func<Task> next)
+        {
+            return _defaultRetryPolicy.ExecuteAsync(next);
+        }
+    }
+}
---
+++ src/NServiceBus.Extensions.DispatchRetries/DispatchRetriesEndpointConfigrationExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NServiceBus.Configuration.AdvancedExtensibility;
+using NServiceBus.Extensions.DispatchRetries;
+using Polly;
+
+namespace NServiceBus
+{
+    public static class DispatchRetriesEndpointConfigrationExtensions
+    {
+        public static void EnableDispatchRetries(this EndpointConfiguration configuration)
+        {
+            var defaultPolicy = Policy.NoOpAsync();
+            EnableDispatchRetries(configuration, defaultPolicy);
+        }
+
+        public static void EnableDispatchRetries(this EndpointConfiguration configuration, AsyncPolicy defaultRetryPolicy)
+        {
+            if (configuration == null)
+            {
+                throw new ArgumentNullException(nameof(configuration));
+            }
+            
+            if (defaultRetryPolicy == null)
+            {
+                throw new ArgumentNullException(nameof(defaultRetryPolicy));
+            }
+            
+            configuration.Pipeline.Register(new DispatchRetriesBehavior(defaultRetryPolicy), "Dispatch retries behavior based on Polly.");
+        }
+    }
+}
