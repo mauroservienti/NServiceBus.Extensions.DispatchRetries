8f381d3e163abfd2a7cc992039df96fa68809d43
mauroservienti <mauro.servienti@gmail.com>
2020-09-16 13:59:35 +0200

    Public API tests

---
+++ src/NServiceBus.Extensions.DispatchRetries.Tests/API/APIApprovals.cs
@@ -0,0 +1,21 @@
+﻿using ApprovalTests;
+using ApprovalTests.Reporters;
+using NUnit.Framework;
+using PublicApiGenerator;
+using System.Runtime.CompilerServices;
+
+namespace NServiceBus.Extensions.DispatchRetries.Tests.API
+{
+    public class APIApprovals
+    {
+        [Test]
+        [UseReporter(typeof(DiffReporter))]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void Approve_API()
+        {
+            var publicApi = ApiGenerator.GeneratePublicApi(typeof(DispatchRetriesEndpointConfigurationExtensions).Assembly, options:null);
+
+            Approvals.Verify(publicApi);
+        }
+    }
+}
