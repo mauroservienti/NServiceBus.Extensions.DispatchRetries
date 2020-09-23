c7f4bce65b109328cc3ddd32796a6844a7b7e947
mauroservienti <mauro.servienti@gmail.com>
2020-09-23 14:17:40 +0200

    Handle dispatch consistency in different scenarios

--- src/NServiceBus.Extensions.DispatchRetries/Behaviors/ImmediateDispatchRetriesBehavior.cs
+++ src/NServiceBus.Extensions.DispatchRetries/Behaviors/ImmediateDispatchRetriesBehavior.cs
@@ -10,24 +10,46 @@ namespace NServiceBus.Extensions.DispatchRetries.Behaviors
 {
     public class ImmediateDispatchRetriesBehavior : Behavior<IDispatchContext>
     {
-        private readonly AsyncPolicy _defaultRetryPolicy;
+        private readonly AsyncPolicy _defaultImmediateRetryPolicy;
+        private readonly AsyncPolicy _defaultBatchRetryPolicy;
 
         public ImmediateDispatchRetriesBehavior(ReadOnlySettings readOnlySettings)
         {
-            readOnlySettings.TryGet(Constants.DefaultImmediateDispatchRetryPolicy, out _defaultRetryPolicy);
+            readOnlySettings.TryGet(Constants.DefaultImmediateDispatchRetryPolicy, out _defaultImmediateRetryPolicy);
+            readOnlySettings.TryGet(Constants.DefaultBatchDispatchRetryPolicy, out _defaultBatchRetryPolicy);
         }
 
         public override Task Invoke(IDispatchContext context, Func<Task> next)
         {
-            var isImmediate = context.Operations.All(op => op.RequiredDispatchConsistency == DispatchConsistency.Isolated);
-            if (isImmediate && context.Extensions.TryGet(Constants.ImmediateDispatchRetryPolicy, out AsyncPolicy retryPolicy))
+            var isIsolatedConsistency = context.Operations.All(op => op.RequiredDispatchConsistency == DispatchConsistency.Isolated);
+            return isIsolatedConsistency ? HandleIsolatedConsistency(context, next) : HandleDefaultConsistency(context, next);
+        }
+
+        Task HandleIsolatedConsistency(IDispatchContext context, Func<Task> next)
+        {
+            if (context.Extensions.TryGet(Constants.ImmediateDispatchRetryPolicy, out AsyncPolicy retryPolicy))
             {
                 return retryPolicy.ExecuteAsync(next);
             }
 
-            if (isImmediate && _defaultRetryPolicy != null)
+            if (_defaultImmediateRetryPolicy != null)
             {
-                return _defaultRetryPolicy.ExecuteAsync(next);
+                return _defaultImmediateRetryPolicy.ExecuteAsync(next);
+            }
+
+            return next();
+        }
+
+        Task HandleDefaultConsistency(IDispatchContext context, Func<Task> next)
+        {
+            if (context.Extensions.TryGet(Constants.BatchDispatchRetryPolicy, out AsyncPolicy retryPolicy))
+            {
+                return retryPolicy.ExecuteAsync(next);
+            }
+
+            if (_defaultBatchRetryPolicy != null)
+            {
+                return _defaultBatchRetryPolicy.ExecuteAsync(next);
             }
 
             return next();
