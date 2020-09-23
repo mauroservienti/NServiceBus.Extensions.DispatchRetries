a87dafbb4d71dc4f3e27eebb6310817c8c33ba1c
mauroservienti <mauro.servienti@gmail.com>
2020-09-23 17:20:12 +0200

    Test using Outbox (docker + postgres)

---
+++ src/NServiceBus.Extensions.DispatchRetries.AcceptanceTests/DockerCompose.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using static SimpleExec.Command;
+
+namespace NServiceBus.Extensions.DispatchRetries.AcceptanceTests
+{
+    public static class DockerCompose
+    {
+        public static async Task Up(Func<Task<bool>> statusChecker = null)
+        {
+            Run("docker-compose", "up -d", workingDirectory: AppDomain.CurrentDomain.BaseDirectory);
+            Run("docker", "ps -a");
+
+            statusChecker ??= () => Task.FromResult(true);
+            while (!await statusChecker())
+            {
+                await Task.Delay(500);
+            }
+        }
+
+        public static void Down()
+        {
+            Run("docker-compose", "down", workingDirectory: AppDomain.CurrentDomain.BaseDirectory);
+        }
+    }
+}
