3171a8579bd4f751236a1d60bd0fcad10bdeb09a
Mauro Servienti <mauro.servienti@gmail.com>
2024-10-18 18:20:28 +0200

    Support resilience pipelines when batch dispatch

--- src/NServiceBus.Extensions.DispatchRetries/Behaviors/BatchDispatchRetriesBehavior.cs
+++ src/NServiceBus.Extensions.DispatchRetries/Behaviors/BatchDispatchRetriesBehavior.cs
@@ -9,10 +9,17 @@ namespace NServiceBus.Extensions.DispatchRetries.Behaviors
     class BatchDispatchRetriesBehavior : Behavior<IBatchDispatchContext>
     {
         private readonly AsyncPolicy _defaultRetryPolicy;
+        private readonly ResiliencePipeline _defaultRetryResiliencePipeline;
 
         public BatchDispatchRetriesBehavior(IReadOnlySettings readOnlySettings)
         {
             readOnlySettings.TryGet(Constants.DefaultBatchDispatchRetryPolicy, out _defaultRetryPolicy);
+            readOnlySettings.TryGet(Constants.DefaultBatchDispatchRetryResiliencePipeline, out _defaultRetryResiliencePipeline);
+
+            if (_defaultRetryPolicy != null && _defaultRetryResiliencePipeline != null)
+            {
+                //TODO warn if both a Policy and a ResiliencePipeline are set for the same dispatch mode and inform that only the ResiliencePipeline will be used    
+            }
         }
 
         public override Task Invoke(IBatchDispatchContext context, Func<Task> next)
@@ -24,7 +31,11 @@ namespace NServiceBus.Extensions.DispatchRetries.Behaviors
                 return overrides.BatchDispatchPolicyOverride.ExecuteAsync(next);
             }
 
-            //TODO: Add support for the resilience pipelines
+            if (_defaultRetryResiliencePipeline != null)
+            {
+                return _defaultRetryResiliencePipeline.ExecuteAsync(_=>new ValueTask(next()), context.CancellationToken).AsTask();
+            }
+            
             if (_defaultRetryPolicy!= null)
             {
                 return _defaultRetryPolicy.ExecuteAsync(next);
